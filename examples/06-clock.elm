9749280cda95c8e2b186bc06efc1372530592694
Aaron L. Zeng <a2z@mit.edu>
2017-02-14 20:31:24 -0500

    Exercise: add a pause button

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html)
+import Html exposing (Html, button, div)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
@@ -18,12 +19,15 @@ main =
 -- MODEL
 
 
-type alias Model = Time
+type alias Model =
+  { time : Time
+  , paused : Bool
+  }
 
 
 init : (Model, Cmd Msg)
 init =
-  (0, Cmd.none)
+  (Model 0 False, Cmd.none)
 
 
 
@@ -32,13 +36,16 @@ init =
 
 type Msg
   = Tick Time
+  | TogglePause
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      ({ model | time = newTime }, Cmd.none)
+    TogglePause ->
+      ({ model | paused = not model.paused }, Cmd.none)
 
 
 
@@ -47,7 +54,10 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every (second / 6) Tick
+  if model.paused then
+    Sub.none
+  else
+    Time.every (second / 6) Tick
 
 
 
@@ -64,11 +74,21 @@ hand length color numTurns =
   line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke color ] []
 
 
+pauseButton : Model -> Html Msg
+pauseButton model =
+  button
+    [ onClick TogglePause ]
+    [ text (if model.paused then "Unpause" else "Pause") ]
+
+
 view : Model -> Html Msg
 view model =
-  svg [ viewBox "0 0 100 100", width "300px" ]
-    [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-    , hand 20 "#023963" (Time.inHours model / 24)
-    , hand 30 "gray" (Time.inHours model)
-    , hand 40 "red" (Time.inMinutes model)
+  div []
+    [ svg [ viewBox "0 0 100 100", width "300px" ]
+      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+      , hand 20 "#023963" (Time.inHours model.time / 24)
+      , hand 30 "gray" (Time.inHours model.time)
+      , hand 40 "red" (Time.inMinutes model.time)
+      ]
+    , pauseButton model
     ]
