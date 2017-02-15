e0872b2042d35981fac305e0bdfef5aefa38779d
Aaron L. Zeng <a2z@mit.edu>
2017-02-14 20:26:23 -0500

    Exercise: add hour/minute hands and display time correctly

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -47,26 +47,28 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+  Time.every (second / 6) Tick
 
 
 
 -- VIEW
 
 
+hand : Float -> String -> Float -> Svg Msg
+hand length color numTurns =
+  let
+    angle = turns numTurns
+    handX = toString (50 + length * sin angle)
+    handY = toString (50 - length * cos angle)
+  in
+  line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke color ] []
+
+
 view : Model -> Html Msg
 view model =
-  let
-    angle =
-      turns (Time.inMinutes model)
-
-    handX =
-      toString (50 + 40 * cos angle)
-
-    handY =
-      toString (50 + 40 * sin angle)
-  in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-      ]
+  svg [ viewBox "0 0 100 100", width "300px" ]
+    [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+    , hand 20 "#023963" (Time.inHours model / 24)
+    , hand 30 "gray" (Time.inHours model)
+    , hand 40 "red" (Time.inMinutes model)
+    ]
